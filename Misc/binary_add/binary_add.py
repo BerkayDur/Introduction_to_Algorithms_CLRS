6867f558de20d8ea561cd3f4df6e497961cb1ff1
BerkayDur <berkaydur20@gmail.com>
2024-03-24 19:53:02 +0000

    Refactor binary add

--- Misc/binary_add/binary_add.py
+++ Misc/binary_add/binary_add.py
@@ -1,37 +1,35 @@
 """Source code for binary add 2.1-5 CLRS"""
 
-def binary_add(binary_array_1: list[int], binary_array_2: list[int]) -> list[int]:
-  """assume binary_array_1 and binary_array_2 are the same length"""
-  output_len = len(binary_array_1) + 1
-  sum_binary_arrays = [0] * output_len
-  for i in range(output_len-2, -1, -1):
-    sum_binary_arrays[i+1] += binary_array_1[i] + binary_array_2[i]
-    sum_binary_arrays[i] = sum_binary_arrays[i+1] // 2
-    sum_binary_arrays[i+1] %= 2
-  return sum_binary_arrays
+# Helper(s)
+def remove_zeros_from_end(binary_array: list[int]) -> list[int]:
+  """Removes extra zeros from the end of a list of numbers.
+  
+  Generally used in the context of a reversed binary array to truncate."""
+  while len(binary_array) > 1 and binary_array[-1] == 0:
+    binary_array.pop()
+  return binary_array
 
-def binary_add_var_len(binary_array_1: list[int], binary_array_2: list[int]) -> list[int]:
+def binary_add(binary_array_1: list[int], binary_array_2: list[int]) -> list[int]:
   """binary add variable length arrays"""
   sum_binary_arrays_rev = [0]
-  i = len(binary_array_1) - 1
-  j = len(binary_array_2) - 1
-  k = 0
+  arr1_count = len(binary_array_1) - 1
+  arr2_count = len(binary_array_2) - 1
+  sum_array_counter = 0
   while True:
-    if i < 0 and j < 0:
-      if sum_binary_arrays_rev[-1] == 0:
-        sum_binary_arrays_rev.pop()
+    if arr1_count < 0 and arr2_count < 0:
+      sum_binary_arrays_rev = remove_zeros_from_end(sum_binary_arrays_rev)
       sum_binary_arrays_rev.reverse()
       return sum_binary_arrays_rev
     sum_binary_arrays_rev.append(0)
-    if i >= 0:
-      sum_binary_arrays_rev[k] += binary_array_1[i]
-      i -= 1
-    if j >= 0:
-      sum_binary_arrays_rev[k] += binary_array_2[j]
-      j -= 1
-    sum_binary_arrays_rev[k+1] += sum_binary_arrays_rev[k] // 2
-    sum_binary_arrays_rev[k] %= 2
-    k += 1
+    if arr1_count >= 0:
+      sum_binary_arrays_rev[sum_array_counter] += binary_array_1[arr1_count]
+      arr1_count -= 1
+    if arr2_count >= 0:
+      sum_binary_arrays_rev[sum_array_counter] += binary_array_2[arr2_count]
+      arr2_count -= 1
+    sum_binary_arrays_rev[sum_array_counter+1] += sum_binary_arrays_rev[sum_array_counter] // 2
+    sum_binary_arrays_rev[sum_array_counter] %= 2
+    sum_array_counter += 1
 
 if __name__ == "__main__":
-  print(binary_add_var_len([0], [0]))
+  print(binary_add([0], [0]))
--- Misc/binary_add/test_binary_add.py
+++ Misc/binary_add/test_binary_add.py
@@ -1,28 +1,33 @@
 # pylint: skip-file
 
-from binary_add import binary_add, binary_add_var_len
+
+from binary_add import binary_add, remove_zeros_from_end
+
+
+def test_remove_zeros_from_end_correct():
+  assert remove_zeros_from_end([0, 0, 0]) == [0]
+
+
+def test_remove_zeros_from_end_empty():
+  assert remove_zeros_from_end([]) == []
+
+def test_remove_zeros_from_end_10000():
+  assert remove_zeros_from_end([1, 0, 0, 0, 0]) == [1]
+
+def test_remove_zeros_from_end_000001():
+  assert remove_zeros_from_end([0, 0, 0, 0, 0, 1]) == [0, 0, 0, 0, 0, 1]
 
 def test_binary_add_zeros():
-  assert binary_add([0], [0]) == [0, 0]
+  assert binary_add([0], [0, 0, 0, 0]) == [0]
 
 def test_binary_add_one_zero():
-  assert binary_add([1], [0]) == [0, 1]
+  assert binary_add([0, 0, 0, 1], [0]) == [1]
+
 
 def test_binary_add_16_3():
-  assert binary_add([1, 0, 0, 0, 0], [0, 0, 0, 1, 1]) == [0, 1, 0, 0, 1, 1]
+  assert binary_add([1, 0, 0, 0, 0], [0, 1, 1]) == [1, 0, 0, 1, 1]
+
 
 def test_binary_add_3_3():
   assert binary_add([1, 1], [1, 1]) == [1, 1, 0]
 
-
-def test_binary_add_var_len_zeros():
-  assert binary_add_var_len([0], [0]) == [0]
-
-def test_binary_add_var_len_one_zero():
-  assert binary_add_var_len([1], [0]) == [1]
-
-def test_binary_add_var_len_16_3():
-  assert binary_add_var_len([1, 0, 0, 0, 0], [1, 1]) == [1, 0, 0, 1, 1]
-
-def test_binary_add_var_len_3_3():
-  assert binary_add_var_len([1, 1], [1, 1]) == [1, 1, 0]
